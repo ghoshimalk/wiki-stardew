49393fe223fc6fb0153fc7d2806baf0f333024b0
ghoshimalk <yaoientoutgenre@outlook.fr>
2026-06-22 18:16:33 +0200

    ajout printemp de chou friser a rhubarbe

--- culture.js
+++ culture.js
@@ -9,4 +9,13 @@ let saison = {
 let printemps = {
     ail:  "Disponible qu'a partire de le 2éme année\n -prix: 40po\n -temps de croissance: 4 jours\n se revend\n -basic: 60po\n -argent: 75po\n -or: 90po\n -légendaire: 120po ",
     carrote: "À creuser uniquement\n -temps de croissance: 3 jours\nSe revend\n -basique: 35po\n -argent: 43po\n -52po\n -or: 52po\n -légendaire: 70po",
+    chouFrisé: " -prix: 70po\n -temps de croissance: 6jours\nse vend\n -basique: 110po\n -argent: 137po\n -or: 165po\n -légendaire: 220po",
+    choueFleur: "Peut devenir une culture géante\n -prix: 80\n -temps de croissance: 12 jours\nse revend\n -basique: 175po\n -argent: 218po\n -or: 262po\n -légendaire: 350po",
+    fraise: " -prix: 100\n -temp de croissance: 8jours\n -repousse tout les 4 jours avec une faible chance d'avoire une fraise supplémentaire\nse revend\n -basique: 120po\n -argent: 150po\n -or: 180po\n -légendaire: 240po",
+    grainDeCafé: "récupérable uniquement au chariot de voyage et 1% de chance a l'esprit de poussière\n -prix: 2 500po\n -temps de croissance: 10 jours\n -repousse tout les 2jours\nse revend\n -basique: 15po\n -argent: 18po\n -or: 22po\n -légendaire: 30po",
+    haricotVert: " -prix: 60po\n -temps de croissance: 10 jours\n -repousse tout les 3 jours\nse revend\n -basique: 40po\n -argent: 50po\n -or: 60po\n -légendaire: 80po",
+    panais: " -prix: 20po\n -temps de croissance: 4 jours\nse revend\n -basique: 35po\n -argent: 43po\n -or: 52 po\n -légendaire: 70po",
+    pommeDeTerre: " -prix: 50po\n -temps de croissance: 6jours\nse revend\n -basique: 80po\n -argent: 100po\n -or: 120po\n -légendaire: 160po",
+    rhubarbe: "Achat chez Oasis\n -prix 100po\n -temps de croissance: 13jours\nse revend\n -basique: 220po\n -argents: 275po\n -or: 330po\n -légendaire: 440po",
+    
 }
