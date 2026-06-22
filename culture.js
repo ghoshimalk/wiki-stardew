f2af962ae18df0c99a4c25c3cf2aad7a1c67307d
ghoshimalk <yaoientoutgenre@outlook.fr>
2026-06-22 02:25:23 +0200

    saison

---
+++ culture.js
@@ -0,0 +1,8 @@
+let saison = {
+    printemps: ["Ail", "Carotte", "Chou frisé", "Chou-fleur", "Fraise", "Grain de café", "Haricot vert", "Panais", "pomme de terre", 
+        "Rhubarbe", "Riz non moulu", "Tulipe", "Zinnia bleue"] ,
+    été: ["Blé", "Carambole", "Chou rouge", "courge d'été", "Fleur tropicale d'été", "Houblon", "Maïs", "Melon", "Mirtylle", "Pavot", "Piment fort", "Radis", "Tomate", "Tournesole"],
+    automne: ["Amarante", "Artichaut", "Betterave", "Brocoli", "Canneberges", "Chou chinois", "Citrouille", "Igname", "Raisin", "Rose de féé"],
+    hiver: ["Poudremelon"],
+    spécial: ["Baie de gemme sucrée", "Feuille de thé", "Fruit ancient", "Fruit de cactus", ]
+}
