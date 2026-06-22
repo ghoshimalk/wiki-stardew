e38dd54bd104c33c4b88355702c57381e69b56c0
ghoshimalk <yaoientoutgenre@outlook.fr>
2026-06-22 10:59:32 +0200

    teste sans tableau

--- culture.js
+++ culture.js
@@ -1,8 +1,13 @@
 let saison = {
     printemps: ["Ail", "Carotte", "Chou frisé", "Chou-fleur", "Fraise", "Grain de café", "Haricot vert", "Panais", "pomme de terre", 
         "Rhubarbe", "Riz non moulu", "Tulipe", "Zinnia bleue"] ,
-    été: ["Blé", "Carambole", "Chou rouge", "courge d'été", "Fleur tropicale d'été", "Houblon", "Maïs", "Melon", "Mirtylle", "Pavot", "Piment fort", "Radis", "Tomate", "Tournesole"],
+    été: ["Blé", "Carambole", "Chou rouge", "courge d'été", "Fleur tropicale d'été", "Houblon", "Maïs", "Melon", "Mirtylle", "Pavot", 
+        "Piment fort", "Radis", "Tomate", "Tournesole"],
     automne: ["Amarante", "Artichaut", "Betterave", "Brocoli", "Canneberges", "Chou chinois", "Citrouille", "Igname", "Raisin", "Rose de féé"],
     hiver: ["Poudremelon"],
     spécial: ["Baie de gemme sucrée", "Feuille de thé", "Fruit ancient", "Fruit de cactus", ]
+}
+
+for (let nomSaison in saison) {
+    console.log(nomSaison);
 }
