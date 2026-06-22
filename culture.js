39772b7bb7e8f95e4eab4f4e63e3f3f62bfb8379
ghoshimalk <yaoientoutgenre@outlook.fr>
2026-06-22 11:57:02 +0200

    printemp ail et carrotte

--- culture.js
+++ culture.js
@@ -1,10 +1,12 @@
 let saison = {
-    printemps: ["Ail", "Carotte", "Chou frisé", "Chou-fleur", "Fraise", "Grain de café", "Haricot vert", "Panais", "pomme de terre", 
-        "Rhubarbe", "Riz non moulu", "Tulipe", "Zinnia bleue"] ,
-    été: ["Blé", "Carambole", "Chou rouge", "courge d'été", "Fleur tropicale d'été", "Houblon", "Maïs", "Melon", "Mirtylle", "Pavot", 
-        "Piment fort", "Radis", "Tomate", "Tournesole"],
-    automne: ["Amarante", "Artichaut", "Betterave", "Brocoli", "Canneberges", "Chou chinois", "Citrouille", "Igname", "Raisin", "Rose de féé"],
-    hiver: ["Poudremelon"],
-    spécial: ["Baie de gemme sucrée", "Feuille de thé", "Fruit ancient", "Fruit de cactus", ]
+    printemps: " -Ail\n -Carotte\n -Chou frisé\n -Chou-fleur\n -Fraise\n -Grain de café\n -Haricot vert\n -Panais\n -pomme de terre\n -Rhubarbe\n -Riz non moulu\n -Tulipe\n -Zinnia bleue" ,
+    été: " -Blé\n -Carambole\n -Chou rouge\n -courge d'été\n -Fleur tropicale d'été\n -Houblon\n -Maïs\n -Melon\n -Mirtylle\n -Pavot\n -Piment fort\n -Radis\n -Tomate\n -Tournesole",
+    automne: " -Amarante\n -Artichaut\n -Betterave\n -Brocoli\n -Canneberges\n -Chou chinois\n -Citrouille\n -Igname\n -Raisin\n -Rose de féé",
+    hiver: " -Poudremelon",
+    spécial: " -Baie de gemme sucrée\n -Feuille de thé\n -Fruit ancient\n -Fruit de cactus"
 }
 
+let printemps = {
+    ail:  "Disponible qu'a partire de le 2éme année\n -prix: 40po\n -temps de croissance: 4 jours\n se revend\n -basic: 60po\n -argent: 75po\n -or: 90po\n -légendaire: 120po ",
+    carrote: "À creuser uniquement\n -temps de croissance: 3 jours\nSe revend\n -basique: 35po\n -argent: 43po\n -52po\n -or: 52po\n -légendaire: 70po",
+}
