c8488f656646881ac344595f1e3cc62373dd36c4
ghoshimalk <yaoientoutgenre@outlook.fr>
2026-06-22 02:45:11 +0200

    rajout des accent

--- culture.js
+++ culture.js
@@ -1,8 +1,8 @@
 let saison = {
     printemps: ["Ail", "Carotte", "Chou frisé", "Chou-fleur", "Fraise", "Grain de café", "Haricot vert", "Panais", "pomme de terre", 
         "Rhubarbe", "Riz non moulu", "Tulipe", "Zinnia bleue"] ,
-    ete: ["Blé", "Carambole", "Chou rouge", "courge d'été", "Fleur tropicale d'été", "Houblon", "Maïs", "Melon", "Mirtylle", "Pavot", "Piment fort", "Radis", "Tomate", "Tournesole"],
+    été: ["Blé", "Carambole", "Chou rouge", "courge d'été", "Fleur tropicale d'été", "Houblon", "Maïs", "Melon", "Mirtylle", "Pavot", "Piment fort", "Radis", "Tomate", "Tournesole"],
     automne: ["Amarante", "Artichaut", "Betterave", "Brocoli", "Canneberges", "Chou chinois", "Citrouille", "Igname", "Raisin", "Rose de féé"],
     hiver: ["Poudremelon"],
-    special: ["Baie de gemme sucrée", "Feuille de thé", "Fruit ancient", "Fruit de cactus", ]
+    spécial: ["Baie de gemme sucrée", "Feuille de thé", "Fruit ancient", "Fruit de cactus", ]
 }
