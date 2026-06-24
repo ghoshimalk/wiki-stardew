9e7b8eb481318a26db0788e071026a2a29fb8b21
ghoshimalk <yaoientoutgenre@outlook.fr>
2026-06-24 20:55:37 +0200

    automne et remplacer - par • pour les qualites

--- culture.js
+++ culture.js
@@ -1,7 +1,7 @@
 let saison = {
     printemps: " -Ail\n -Carotte\n -Chou frisé\n -Chou-fleur\n -Fraise\n -Grain de café\n -Haricot vert\n -Panais\n -pomme de terre\n -Rhubarbe\n -Riz non moulu\n -Tulipe\n -Zinnia bleue" ,
     été: " -Blé\n -Carambole\n -Chou rouge\n -courge d'été\n -Fleur tropicale d'été\n -Houblon\n -Maïs\n -Melon\n -Mirtylle\n -Pavot\n -Piment fort\n -Radis\n -Tomate\n -Tournesole",
-    automne: " -Amarante\n -Artichaut\n -Betterave\n -blé\n -Brocoli\n -Canneberges\n -Chou chinois\n -Citrouille\n -Igname\n -Raisin\n -Rose de féé",
+    automne: " -Amarante\n -Artichaut\n -Betterave\n -blé\n -Brocoli\n -Canneberges\n -Chou chinois\n -Citrouille\n -Igname\n -Raisin\n -Rose de féé\n Tournsesol",
     hiver: " -Poudremelon",
     spécial: " -Baie de gemme sucrée\n -Feuille de thé\n -Fruit ancient\n -Fruit de cactus"
 }
@@ -23,18 +23,33 @@ let printemps = {
 }
 
 let été = {
-    blé: " -prix: 10po\n -tempd de croissance: 4jours\nse revend\n -basique: 25po\n -argent: 31po\n -or: 37po\n -légendaire: 50po",
-    carambole: "Achat uniquement chez Oasis\n -prix: 400po\n -temps de pousse: 13jours\nSe revend\n -basique: 750po\n -argent: 937po\n -or: 1 125po\n -légendaire: 1 500po",
-    chouRouge: "Disponible qu'à partire de la deuxième année\n -prix: 100po\n -temps de pousse: 9 jours\nse revend\n -basique: 260po\n -argent: 325po\n -or: 390po\n -légendaire: 520po",
-    courgeDété: "À creuser uniquement\n -temps de pousse: 6 jours\n -repousse tout les 3 jours\nse revend\n -basique: 45po\n -argent: 56po\n -or: 67po\n -légandaire: 90po",
-    fleurTropicaleDété: " -prix: 50po\n -temps de pousse: 8 jours\nse revend\n -basique: 90po\n -argent: 112po\n -or: 135po\n -légendaire: 180po",
-    houblon: " -prix: 60po\n -temps de pousse: 11 jours\n -repousse tout les jours\nse revend\n -basique: 25po\n -argent 31po\n -or: 37po\n -légendaire: 50po",
-    maïs: " -prix: 150po\n -temps de pousse: 14 jours\n -repousse tout les 4 jours\nse revend\n -basique: 50po\n -argent: 62po\n -or: 75po\n -légendaire: 100po",
-    melon: "Peux devenir une culture géante\n -prix: 80po\n -temps de pousse: 12 jours\nse revend\n -basique: 250po\n -argent: 312po\n -or: 375po\n -légendaire: 500po",
-    myrtille: "≈2% de chance de produire plus de myrtille\n -prix: 80po\n -temps de pousse: 13 jours\n -repousse tout les 4 jours\nse revend\n -basique: 50po\n -argent: 62po\n -or: 75po\n -légendaire: 100po",
-    pavot: " -prix: 100po\n -temps de pousse: 7 jours\nse revend\n -basique: 140po\n -argent: 175po\n -or: 210po\n -légendaire: 280po",
-    pimentFort: " -prix: 40po\n -temps de pousse: 5 jours\n -repousse tout les 3 jours\nse revend\n -basique: 40po\n -argent: 50po\n -or: 60po\n -légendaire: 80po",
-    radis: " -prix: 40po\n -temps de pousse: 6 jours\nse revend\n -basique: 90po\n -argent: 112po\n -or: 135po\n -légendaire: 180po",
-    tomate: " -prix: 50po\n -temps de pousse: 11 jours\n -repousse tout les 4 jours\nse revend\n -basique: 60po\n -argent: 75po\n -or: 90po\n -légendaire: 120po",
-    tournesol: "Produira également de 0 à 3 graine de tournesol\n -prix: 200po\n -temps de pousse: 8 jours\nse revend\n -basique: 80po\n -argent: 100po\n -or: 120po\n -légendaire: 160po",
+    blé: " -prix: 10po\n -tempd de croissance: 4jours\nse revend\n •basique: 25po\n •argent: 31po\n •or: 37po\n •légendaire: 50po",
+    carambole: "Achat uniquement chez Oasis\n -prix: 400po\n -temps de pousse: 13jours\nSe revend\n •basique: 750po\n •argent: 937po\n •or: 1 125po\n •légendaire: 1 500po",
+    chouRouge: "Disponible qu'à partire de la deuxième année\n -prix: 100po\n -temps de pousse: 9 jours\nse revend\n •basique: 260po\n •argent: 325po\n •or: 390po\n •légendaire: 520po",
+    courgeDété: "À creuser uniquement\n -temps de pousse: 6 jours\n -repousse tout les 3 jours\nse revend\n •basique: 45po\n •argent: 56po\n •or: 67po\n •légandaire: 90po",
+    fleurTropicaleDété: " -prix: 50po\n -temps de pousse: 8 jours\nse revend\n -basique: 90po\n •argent: 112po\n •or: 135po\n •légendaire: 180po",
+    houblon: " -prix: 60po\n -temps de pousse: 11 jours\n -repousse tout les jours\nse revend\n •basique: 25po\n •argent 31po\n •or: 37po\n •légendaire: 50po",
+    maïs: " -prix: 150po\n -temps de pousse: 14 jours\n -repousse tout les 4 jours\nse revend\n •basique: 50po\n •argent: 62po\n •or: 75po\n •légendaire: 100po",
+    melon: "Peux devenir une culture géante\n -prix: 80po\n -temps de pousse: 12 jours\nse revend\n •basique: 250po\n •argent: 312po\n •or: 375po\n •légendaire: 500po",
+    myrtille: "≈2% de chance de produire plus de myrtille\n -prix: 80po\n -temps de pousse: 13 jours\n -repousse tout les 4 jours\nse revend\n •basique: 50po\n •argent: 62po\n •or: 75po\n •légendaire: 100po",
+    pavot: " -prix: 100po\n -temps de pousse: 7 jours\nse revend\n •basique: 140po\n •argent: 175po\n •or: 210po\n •légendaire: 280po",
+    pimentFort: " -prix: 40po\n -temps de pousse: 5 jours\n -repousse tout les 3 jours\nse revend\n •basique: 40po\n •argent: 50po\n •or: 60po\n •légendaire: 80po",
+    radis: " -prix: 40po\n -temps de pousse: 6 jours\nse revend\n -basique: 90po\n •argent: 112po\n •or: 135po\n •légendaire: 180po",
+    tomate: " -prix: 50po\n -temps de pousse: 11 jours\n -repousse tout les 4 jours\nse revend\n -basique: 60po\n •argent: 75po\n •or: 90po\n •légendaire: 120po",
+    tournesol: "Produira également de 0 à 3 graine de tournesol\n -prix: 200po\n -temps de pousse: 8 jours\nse revend\n •basique: 80po\n •argent: 100po\n •or: 120po\n •légendaire: 160po",
+}
+
+let automne = {
+    amarante: " -prix: 70po\n -temps de pousse: 7 jours\nse revend\n -basique: 150po\n -argent: 187po\n -or: 225po\n -légendaire: 300po",
+    artichaut: "Disponible à partire de la deuxième année\n -prix: 30po\n -temps de pousse: 8 jours\nse revend\n •basique: 160po\n •argent: 200po\n •or: 240po\n •légendaire: 320po",
+    aubergine: " -prix: 20po\n -temps de pousse: 5 jours\n -repousse tout les 5 jours\nse revend\n •basique: 60po\n •argent: 75po\n •or: 90po\n •légendaire: 120po",
+    bettrave: "Achat uniquement chez Oasis\n -prix: 20po\n -temps de pousse: 6 jours\nse revend\n •basique: 100po\n •argent: 125po\n •or: 150po\n •légendaire: 200po",
+    brocoli: "À creuser uniquement\n -temps de pousse: 8 jours\n -repousse tout les 4 jours\nse revend\n •basique: 70po\n •argent: 87po\n •or: 105\n •légendaire: 140po",
+    cannberges: " -prix: 240po\n -temps de pousse: 7 jours\n -repouse tout les 5 jours\nse revend\n •basique: 75po\n •argent: 93po\n •or: 112po\n •légendaire: 150po",
+    chouChinois: " -prix: 50po\n -temps de pusse: 4 jours\nse revend\n •basique: 80po\n •argent: 100po\n •or: 120po\n •légendaire: 160po",
+    citrouille: "Peux devenir une culture géante\n -prix: 100po\n -temps de pousse: 13 jours\nse revend\n •basique: 320po\n •argent 400po\n •or: 180po\n •légendaire: 640po",
+    igname: " -prix: 60po\n -temps de pousse: 10 jours:\nse revend\n •basique: 160po\n •argent: 200po\n •or: 240po\n •légendaire: 320po",
+    raisin: " -prix: 60po\n -temps de pousse: 10 jours\n -repousse tout les 3 jours\nse revend\n •basique: 80po\n •argent: 100\n •or: 120po\n •légendaire: 160po",
+    roseDeFée: " -prix: 200po\n -temps de pousse 12 jours\nse revend\n •basique: 290po\n •argent: 362po\n •or: 435po\n •légendaire: 580po",
+    tournesol: "Produira également de 0 à 3 graine de tournesol\n -prix: 200po\n -temps de pousse: 8 jours\nse revend\n •basique: 80po\n •argent: 100po\n •or: 120po\n •légendaire: 160po",
 }
